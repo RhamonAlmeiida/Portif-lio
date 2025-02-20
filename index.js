e147919c836e36bafe654bccc05997035cbb4ea4
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-02-19 23:36:34 -0300

    excluido php e JS modificado

--- index.js
+++ index.js
@@ -15,3 +15,35 @@ ouverlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
 
+class FormSubmit {
+    constructor(settings){
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+    }
+
+    displaySuccess() {
+        this.form.innerHTML = this.settings.success;
+    }
+
+    displayError() {
+        this.form.innerHTML = this.settings.error;
+    }
+
+    init() {
+        if (this.form) this.formButton.addEventListener("click", () => this.displaySuccess());
+        return this;
+    }
+}
+
+const FormSubmit = new FormSubmit({
+    form: "[data-form]",
+    button:"[data-button]",
+    success: "<h1 class='succes'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+
+});
+FormSubmit.init();
